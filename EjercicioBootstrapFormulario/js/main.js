6af4d173cc257ec682bcb8515f5ec7403649afca
tezru24 <tomasweiske@gmail.com>
2018-10-17 19:23:49 +0200

    Ejercicio Avanzado

--- EjercicioBootstrapFormulario/js/main.js
+++ EjercicioBootstrapFormulario/js/main.js
@@ -4,3 +4,23 @@ function onSubmit( form ){
     console.log( data );
     return false; //don't submit
   }
+
+window.onload = function() {
+  var date = new Date();
+
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+
+  var today = year + "-" + month + "-" + day;       
+  document.getElementById('fecha').value = today;
+  document.getElementById('fecha-incidente').value = today;
+  document.getElementById('d2').value = today;
+  document.getElementById('margen2').value = today;
+  document.getElementById('margen3').value = today;
+  document.getElementById('d5').value = today;
+  document.getElementById('d6').value = today;
+}
